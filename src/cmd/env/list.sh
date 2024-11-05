677b237e3e460470c29297904c1cf8cea4612594
Daniel F. Rivera C. <dsoftcolombia@gmail.com>
2024-11-05 05:23:19 -0500

    Close support/env-list-cmd
    - Update env:list command
    
    - Update env:list documentation

--- src/cmd/env/list.sh
+++ src/cmd/env/list.sh
@@ -5,19 +5,17 @@
 source ~/.magetools/src/bootstrap.sh
 
 # Declare variables
-declare cmd_name="env:list"
-declare docs=$(find /magento-app -maxdepth 4 -type f -name magento)
-declare doc
-declare cmd
+cmd_name="env:list"
+available_envs=$(find ${path_env} -maxdepth 4 -type f -name magento)
 
 # Fragment: show-help
-source ~/.magetools/src/fragments/show-help.sh
+source ${path_fragments}/show-help.sh
 
 # Display commands list
 info_message "Available envs"
-for doc in ${docs[@]}; do
-    cmd=$(echo ${doc} | sed -e "s|/magento-app/||" | sed -e "s|/site/bin/magento||")
-    custom_message "    - ${cmd}"
+for available_env in ${available_envs[@]}; do
+    env_name=$(echo ${available_env} | sed -e "s|/magento-app/||" | sed -e "s|/site/bin/magento||")
+    custom_message "    - ${env_name}"
 done
 
 # End script
