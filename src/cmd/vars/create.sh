2b66ff55d1421de53e20507c64eb5c3ad236b462
Daniel F. Rivera C. <dsoftcolombia@gmail.com>
2024-11-07 08:08:49 -0500

    Close support/vars-create-cmd
    - Update vars:create cmd
    
    - Update vars:create docs

--- src/cmd/vars/create.sh
+++ src/cmd/vars/create.sh
@@ -4,39 +4,31 @@
 [ ! -f ~/.magetools/src/bootstrap.sh ] && error_message "Can not bootstrap magetools" && exit 1
 source ~/.magetools/src/bootstrap.sh
 
-# Check params
-declare must_force=0
-while [ "${1}" == "--help" ] || [ "${1}" == "--force" ]; do
-    if [ "${1}" == "--help" ]; then
-        [ ! -f ~/.magetools/src/docs/vars:create.txt ] && error_message "No help documentation found" && exit 1
-        (cat ~/.magetools/src/docs/vars:create.txt | less -c) && exit 0
-    elif [ "${1}" == "--force" ]; then
-        must_force=1
-        shift
-    fi
-done
+# Declare global variables
+cmd_name="vars:create"
+env_name=$(bash ${path_scripts}/get-env-name.sh ${@})
 
-# Check final args
-[ ${#} -ne 1 ] && error_message "Syntax error when executing ${color_yellow}vars:create${color_none} type ${color_yellow}mage vars:create --help${color_none} to see the right syntax" && exit 1
+# Fragment: show-help
+source ${path_fragments}/show-help.sh
 
-# Check env name
-[ -z "${1}" ] && error_message "Env name must be provided" && exit 1
+# Fragment: validate-options
+source ${path_fragments}/validate-options.sh
 
 # Check var directory
-[ ! -d ~/.magetools/var ] && mkdir -vp ~/.magetools/var
+[ ! -d ${path_var} ] && mkdir -vp ${path_var}
 
 # Check if file exists
-if [ ${must_force} -eq 0 ]; then
-    [ -f ~/.magetools/var/vars-"${1}".sh ] && error_message "Vars file for env with name ${color_yellow}${1}${color_none} already exists" && exit 1
+if [ ${cmd_use_force} -eq 0 ]; then
+    [ -f ${path_var}/vars-"${env_name}".sh ] && error_message "Vars file for env with name ${color_yellow}${env_name}${color_none} already exists" && exit 1
 else
-    rm -rfv ~/.magetools/var/vars-"${1}".sh
+    rm -rfv ${path_var}/vars-"${env_name}".sh
 fi
 
 # Copy new file
-cp -v ~/.magetools/src/templates/vars.sh.sample ~/.magetools/var/vars-"${1}".sh
+cp -v ${path_templates}/vars.sh.sample ${path_var}/vars-"${env_name}".sh
 
 # Open for edit new vars file
-nano ~/.magetools/var/vars-"${1}".sh
+nano ${path_var}/vars-"${env_name}".sh
 
 # End process
 exit 0
